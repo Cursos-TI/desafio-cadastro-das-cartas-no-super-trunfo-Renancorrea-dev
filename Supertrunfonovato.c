6697718e092e8d2a08b64315ddedfd0f007a56c1
Renan Corrêa <devrenancorrea@gmail.com>
2025-07-27 13:24:19 +0000

    Desafio cadastro de cartas super trunfo novato

--- Supertrunfonovato.c
+++ Supertrunfonovato.c
@@ -18,6 +18,15 @@ int main (){
     printf("Digite os Pontos turisticos: \n");
     scanf("%d", &Pontosturisticos);
 
+    // Preciso de ajuda, estou bem perdido
+    // O material de estudo esta bem confuso
+    // em cada material é solicitado que faça esse mesmo exercicio de formas diferentes
+    // aqui no git é pedido apenas o cadastro de duas cartas com as funçoes printf e scanf e as variaveis int e float
+    // no material ditatico ja pede para fazer mais coisas com adição de mais variaveis
+    // na video aula pede ainda mais coisas, tudo isso no nivel novato 
+    // qual material devo seguir ? 
+    // mais um adendo! um exemplo pratico de como fazer o cadastro de varias cartas seria de grande ajuda
+    
     
 
 
