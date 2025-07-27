2a68aced32adb2c3ff56f6d4b98f586991c56b96
Renan Corrêa <devrenancorrea@gmail.com>
2025-07-27 13:04:17 +0000

    Cadastro de cartas para o jogo super trunfo novato

---
+++ Supertrunfonovato.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main (){
+
+    int população;
+    float area;
+    float PIB;
+    int Pontosturisticos; 
+
+    printf("Digite a população: \n");
+    scanf("%d", &população);
+
+    printf("Digite a area: \n");
+    scanf("%d", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%d", &PIB);
+
+    printf("Digite os Pontos turisticos: \n");
+    scanf("%d", &Pontosturisticos);
+
+    
+
+
+
+
+}
