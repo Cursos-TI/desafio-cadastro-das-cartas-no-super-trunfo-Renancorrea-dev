695881425022011b2607606d5c53a8c9aca437b6
Renan Corrêa <devrenancorrea@gmail.com>
2025-08-07 09:32:21 -0300

    Supertrunfonovato.c
    Super trunfo desafio novato.

--- Supertrunfonovato.c
+++ Supertrunfonovato.c
@@ -1,35 +1,89 @@
 #include <stdio.h>
-int main (){
+#include <string.h>
 
-    int população;
-    float area;
-    float PIB;
-    int Pontosturisticos; 
+    int main (){
 
-    printf("Digite a população: \n");
-    scanf("%d", &população);
 
-    printf("Digite a area: \n");
-    scanf("%d", &area);
+    /*Variaveis Carta 1*/
 
-    printf("Digite o PIB: \n");
-    scanf("%d", &PIB);
-
-    printf("Digite os Pontos turisticos: \n");
-    scanf("%d", &Pontosturisticos);
-
-    // Preciso de ajuda, estou bem perdido
-    // O material de estudo esta bem confuso
-    // em cada material é solicitado que faça esse mesmo exercicio de formas diferentes
-    // aqui no git é pedido apenas o cadastro de duas cartas com as funçoes printf e scanf e as variaveis int e float
-    // no material ditatico ja pede para fazer mais coisas com adição de mais variaveis
-    // na video aula pede ainda mais coisas, tudo isso no nivel novato 
-    // qual material devo seguir ? 
-    // mais um adendo! um exemplo pratico de como fazer o cadastro de varias cartas seria de grande ajuda
+    char cidade1 [100];
+    int população1;
+    double area1;
+    double PIB1;
+    int Pontosturisticos1;
     
+    /*Variaveis Carta 2*/
+
+    char cidade2 [100];
+    int população2;
+    double area2;
+    double PIB2;
+    int Pontosturisticos2;
+
+    /* Leitura da carta 1 */
+
+    printf("Digite a cidade 1: " );
+    scanf("%s", cidade1); 
+    //Sem uso de & por causa da string
+
+    printf("Digite a população 1: " );
+    scanf("%d", &população1);
+
+    printf("Digite a área (km²) da Cidade 1: ");    
+    scanf("%lf", &area1); 
+
+    printf("Digite o PIB 1: " );
+    scanf("%lf", &PIB1);
+    // Uso do %.2lf para exibir números double com duas casas decimais.
+
+    printf("Digite os pontos turísticos da Cidade 1: ");
+       scanf("%d", &Pontosturisticos1);
+
+    /*Leitura Carta 2*/
+
+    printf("Digite a cidade 2: ");
+    scanf("%s", cidade2);
+
+    printf("Digite a população 2: ");
+    scanf("%d", &população2);
+
+    printf("Digite a area 2 (km2): ");
+    scanf("%lf", &area2);
+
+    printf("Digite o PIB 2: ");
+    scanf("%lf", &PIB2);
+
+    printf("Digite os pontosturisticos1: ");
+    scanf("%d", &Pontosturisticos2);
+
+    /*Dados primeira carta*/
     
+    printf("\\n--- Dados da CARTA 1 ---\\n");
+    
+    printf("nome: %s\\n", cidade1);
+    
+    printf("população: %d\\n", população1);
+    
+    printf("area: %.2lf km²\\n", area1);
+    // %.2lf para exibir double com 2 casas decimais
+    
+    printf("PIB: %.2lf\\n", PIB1);    
+    
+    printf("Pontos Turísticos: %d\\n", Pontosturisticos1);
+         
+    /*Dados segunda carta*/
 
-
-
-
-}
+    
+    printf("\\n--- Dados da CARTA 2 ---\\n");    
+    
+    printf("nome: %s\\n", cidade2);    
+    
+    printf("população: %d\\n", população2);    
+    
+    printf("area: %.2lf km²\\n", area2);    
+    
+    printf("PIB: %.2lf\\n", PIB2);    
+    
+    printf("Pontos Turísticos: %d\\n", Pontosturisticos2);
+    
+    return 0;}
